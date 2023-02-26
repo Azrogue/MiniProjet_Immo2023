d714ead41ed13fe9cd3ba2776007b3db1bbd2e5f
AlexandruRaul <alexandrurak74@gmail.com>
2023-02-27 00:10:08 +0100

    ie

--- main.py
+++ main.py
@@ -122,70 +122,81 @@ def ouvrir_ajout_bien_immobilier():
 
     adresse_label = tk.Label(ajout_bien_immobilier_window, text="Adresse :")
     adresse_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
+
+    adresse_nr_label = tk.Label(ajout_bien_immobilier_window, text="N°:")
+    adresse_nr_label.place(x=170, y=55)
     global adresse_nr_entry
     adresse_nr_entry = tk.Entry(ajout_bien_immobilier_window, width=5)
-    adresse_nr_entry.place(x=170, y=55)
+    adresse_nr_entry.place(x=192, y=55)
     
+    adresse_type_voie_label = tk.Label(ajout_bien_immobilier_window, text="Type de voie:")
+    adresse_type_voie_label.place(x=222, y=55)
     global adresse_type_voie_var
     adresse_type_voie_var = tk.StringVar(ajout_bien_immobilier_window)
     addresse_type_voie_choices = ["Rue", "Allée", "Boulevard", "Chemin"]
     adresse_type_voie_var.set(addresse_type_voie_choices[0])
 
     adresse_type_voie_dropdown = tk.OptionMenu(ajout_bien_immobilier_window, adresse_type_voie_var, *addresse_type_voie_choices)
-    adresse_type_voie_dropdown.place(x=202, y=47)
+    adresse_type_voie_dropdown.place(x=295, y=47)
     
+    adresse_nomvoie_label = tk.Label(ajout_bien_immobilier_window, text="Nom de la voie:")
+    adresse_nomvoie_label.grid(row=1, column=3, padx=10, pady=10, sticky=tk.W)
     global adresse_nomvoie_entry
     adresse_nomvoie_entry = tk.Entry(ajout_bien_immobilier_window, width=8)
-    adresse_nomvoie_entry.place(x=262, y=55)
+    adresse_nomvoie_entry.place(x=400, y=55)
 
+    cp_adresse_label = tk.Label(ajout_bien_immobilier_window, text="Code postale:")
+    cp_adresse_label.grid(row=2, column=1, padx=10, pady=10, sticky=tk.W)
     global cp_adresse_entry
     cp_adresse_entry = tk.Entry(ajout_bien_immobilier_window, width=6)
-    cp_adresse_entry.place(x=280, y=55)
+    cp_adresse_entry.place(x=400, y=55)
 
+    nom_ville_adresse_label = tk.Label(ajout_bien_immobilier_window, text="Ville :")
+    nom_ville_adresse_label.grid(row=2, column=2, padx=10, pady=10, sticky=tk.W)
     global nom_ville_adresse_entry
     nom_ville_adresse_entry = tk.Entry(ajout_bien_immobilier_window,width= 8)
-    nom_ville_adresse_entry.place(x=302, y=55)
+    nom_ville_adresse_entry.place(x=400, y=55)
     superficie_couvert_label = tk.Label(ajout_bien_immobilier_window, text="Superficie couverte (m²) :")
-    superficie_couvert_label.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
+    superficie_couvert_label.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
 
     validate_func = ajout_bien_immobilier_window.register(validate_input)
     global superficie_couvert_entry
     superficie_couvert_entry = tk.Entry(ajout_bien_immobilier_window, validate="key", validatecommand=(validate_func, "%P"))
-    superficie_couvert_entry.grid(row=2, column=1, padx=10, pady=10)
+    superficie_couvert_entry.grid(row=3, column=1, padx=10, pady=10)
 
     superficie_jardin_label = tk.Label(ajout_bien_immobilier_window, text="Superficie jardin (m²) :")
-    superficie_jardin_label.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
+    superficie_jardin_label.grid(row=4, column=0, padx=10, pady=10, sticky=tk.W)
 
     validate_func = ajout_bien_immobilier_window.register(validate_input)
     global superficie_jardin_entry
     superficie_jardin_entry = tk.Entry(ajout_bien_immobilier_window, validate="key", validatecommand=(validate_func, "%P"))
-    superficie_jardin_entry.grid(row=3, column=1, padx=10, pady=10)
+    superficie_jardin_entry.grid(row=4, column=1, padx=10, pady=10)
 
     nombre_pieces_label = tk.Label(ajout_bien_immobilier_window, text="Nombre de pièces :")
-    nombre_pieces_label.grid(row=4, column=0, padx=10, pady=10, sticky=tk.W)
+    nombre_pieces_label.grid(row=5, column=0, padx=10, pady=10, sticky=tk.W)
 
     validate_func = ajout_bien_immobilier_window.register(validate_input)
     global nombre_pieces_entry
     nombre_pieces_entry = tk.Entry(ajout_bien_immobilier_window, validate="key", validatecommand=(validate_func, "%P"))
-    nombre_pieces_entry.grid(row=4, column=1, padx=10, pady=10)
+    nombre_pieces_entry.grid(row=6, column=1, padx=10, pady=10)
 
     classe_energetique_label = tk.Label(ajout_bien_immobilier_window, text="Classe énergétique :")
-    classe_energetique_label.grid(row=5, column=0, padx=10, pady=10, sticky=tk.W)
+    classe_energetique_label.grid(row=6, column=0, padx=10, pady=10, sticky=tk.W)
     global classe_energetique_var
     classe_energetique_var = tk.StringVar(ajout_bien_immobilier_window)
     classe_energetique_choices = ["A", "B", "C", "D", "E", "F", "G"]
     classe_energetique_var.set(classe_energetique_choices[0])
 
     classe_energetique_dropdown = tk.OptionMenu(ajout_bien_immobilier_window, classe_energetique_var, *classe_energetique_choices)
-    classe_energetique_dropdown.grid(row=5, column=1, padx=10, pady=10, sticky=tk.W)
+    classe_energetique_dropdown.grid(row=6, column=1, padx=10, pady=10, sticky=tk.W)
 
     annee_construction_label = tk.Label(ajout_bien_immobilier_window, text="Année de construction :")
-    annee_construction_label.grid(row=6, column=0, padx=10, pady=10, sticky=tk.W)
+    annee_construction_label.grid(row=7, column=0, padx=10, pady=10, sticky=tk.W)
 
     validate_func = ajout_bien_immobilier_window.register(validate_input)
     global annee_construction_entry
     annee_construction_entry = tk.Entry(ajout_bien_immobilier_window, validate="key", validatecommand=(validate_func, "%P"))
-    annee_construction_entry.grid(row=6, column=1, padx=10, pady=10)
+    annee_construction_entry.grid(row=7, column=1, padx=10, pady=10)
 
     # Création de boutons radios pour la nature de gestion
     nature_gestion_label = tk.Label(ajout_bien_immobilier_window, text="Nature de la gestion :")
@@ -196,31 +207,31 @@ def ouvrir_ajout_bien_immobilier():
     nature_gestion_value.set("Location")  # Valeur par défaut sélectionnée
 
     location_radio = tk.Radiobutton(ajout_bien_immobilier_window, text="Location", variable=nature_gestion_value, value="Location")
-    location_radio.grid(row=7, column=1, padx=10, pady=10, sticky=tk.W)
+    location_radio.grid(row=8, column=1, padx=10, pady=10, sticky=tk.W)
 
     vente_radio = tk.Radiobutton(ajout_bien_immobilier_window, text="Vente", variable=nature_gestion_value, value="Vente")
-    vente_radio.grid(row=7, column=2, padx=10, pady=10, sticky=tk.W)
+    vente_radio.grid(row=8, column=2, padx=10, pady=10, sticky=tk.W)
 
     #=====A METTRE AUTOMATIQUEMENT AVEC LOCAL.DATE EN SQL===#
     date_mise_marche_label = tk.Label(ajout_bien_immobilier_window, text="Date de mise en marche :")
-    date_mise_marche_label.grid(row=8, column=0, padx=10, pady=10, sticky=tk.W)
+    date_mise_marche_label.grid(row=9, column=0, padx=10, pady=10, sticky=tk.W)
     # Créer un label pour la date courante
     today_label = tk.Label(ajout_bien_immobilier_window, text=date.today().strftime("%d/%m/%Y"))
-    today_label.grid(row=8, column=1, padx=10, pady=10, sticky=tk.E)
+    today_label.grid(row=9, column=1, padx=10, pady=10, sticky=tk.E)
     #date_mise_marche_entry = tk.Entry(ajout_bien_immobilier_window)
     #date_mise_marche_entry.grid(row=8, column=1, padx=10, pady=10)
 
     prix_label = tk.Label(ajout_bien_immobilier_window, text="Prix :")
-    prix_label.grid(row=9, column=0, padx=10, pady=10, sticky=tk.W)
+    prix_label.grid(row=10, column=0, padx=10, pady=10, sticky=tk.W)
 
     validate_func = ajout_bien_immobilier_window.register(validate_input)
     global prix_entry
     prix_entry = tk.Entry(ajout_bien_immobilier_window, validate="key", validatecommand=(validate_func, "%P"))
-    prix_entry.grid(row=9, column=1, padx=10, pady=10)
+    prix_entry.grid(row=10, column=1, padx=10, pady=10)
 
     # Création du bouton de validation
     valider_button = tk.Button(ajout_bien_immobilier_window, text="Valider", command=valider_saisie)
-    valider_button.grid(row=10, column=0, padx=10, pady=10)
+    valider_button.grid(row=11, column=0, padx=10, pady=10)
 
 def recup_data_in_db():
     # Connexion à la base de données
